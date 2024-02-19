4752906a8929b5cd3ac4f32d2702268bd3ec1d8f
Mary Perez <mperez@accumen.com>
2024-02-19 17:16:11 -0600

    MBHS-818 test script

---
+++ mbhschsurgical.mbhpages.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+import LoginPage from './classes/loginPage';
+import DashboardPage from './classes/dashboardPage';
+import WorklistPage from './classes/worklistPage';
+
+test('surgical visit', async ({ page }) => {
+    test.slow();
+    const login = new LoginPage(page);
+
+    await page.goto('https://qa.mybloodhealth.com/login');
+    await login.enterEmail('cts-secure@accumen.com');
+    await login.enterPassword('Iu$24680');
+    await login.clickLoginBtn();
+
+    const dashboard = new DashboardPage(page);
+    await dashboard.clickClientDropDown('QA Testing');
+
+    const worklist = new WorklistPage(page);
+    await worklist.clickWorklist();
+    await worklist.clickSurgical();
+    await worklist.scheduleSurgicalVisit('Existing', 'n/a', 'n/a', 'n/a', 'n/a', 'smith','n/a','n/a','n/a','n/a','n/a', 'n/a',
+    'n/a','n/a', 'n/a','n/a','n/a','2024', 'MAR', '15', 13,
+        'CARDIO', 'test');
+    await worklist.saveScheduledVisit();
+
+
+
+})
