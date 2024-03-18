0efccb340d2d6107a711a796a14fd2d4a881f0ba
Danny Quach <dquach@accumen.com>
2024-03-18 16:14:25 -0400

    Updated for MBHS-861

---
+++ mbhdocumentpreview.mbhpages.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+import LoginPage from './classes/loginPage';
+import DashboardPage from './classes/dashboardPage';
+import WorklistPage from './classes/worklistPage';
+
+test('document preview', async ({ page }) => {
+    test.slow();
+    const login = new LoginPage(page);
+
+    await page.goto('https://qa.mybloodhealth.com/login');
+    await login.enterEmail('cts-secure@accumen.com');
+    await login.enterPassword('Iu$24680');
+    await login.clickLoginBtn();
+
+    const dashboard = new DashboardPage(page);
+    await dashboard.clickClientDropDown('QA Testing');
+
+    const worklist = new WorklistPage(page);
+    await worklist.clickWorklist();
+    await worklist.clickSurgical();
+    await worklist.selectPatientfromSearch('Sam Smyth');
+    await worklist.visitDocumentsAdd();
+    await worklist.addVisitDocuments();
+    await worklist.closeVisitDocuments();
+    await worklist.visitDocumentsPreview();
+
+})
