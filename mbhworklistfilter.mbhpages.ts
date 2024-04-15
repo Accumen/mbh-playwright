6f70a9f79fdb0b8de5768af1d0ad9c17d47fd5cd
Mary Perez <mperez@accumen.com>
2024-04-15 11:22:59 -0500

    Updated for MBHS-876

--- mbhworklistfilter.mbhpages.ts
+++ mbhworklistfilter.mbhpages.ts
@@ -3,7 +3,7 @@ import LoginPage from './classes/loginPage';
 import DashboardPage from './classes/dashboardPage';
 import WorklistPage from './classes/worklistPage';
 
-test('worklist filter test', async ({ page }) => {
+test('worklist filter date range test', async ({ page }) => {
 
   test.slow();
   const login = new LoginPage(page);
@@ -22,4 +22,24 @@ test('worklist filter test', async ({ page }) => {
   await worklist.worklistscreenshot(1);
   await worklist.sortByDateRange('2024','APR','12','2024','APR','13');
   await worklist.worklistscreenshot(2);
+})
+
+test('worklist filter case type test', async ({ page }) => {
+
+  test.slow();
+  const login = new LoginPage(page);
+  await page.goto('https://qa.mybloodhealth.com/login');
+  await login.enterEmail('cts-secure@accumen.com');
+  await login.enterPassword('Pass#123');
+  await login.clickLoginBtn();
+
+  const dashboard = new DashboardPage(page);
+  await dashboard.clickClientDropDown('QA Testing');
+
+  const worklist = new WorklistPage(page);
+  await worklist.clickWorklist();
+  await worklist.clickSurgical();
+  await worklist.unselectAllCaseTypes();
+  await worklist.selectCaseType('THORACIC');
+  await worklist.worklistscreenshot(1);
 })
