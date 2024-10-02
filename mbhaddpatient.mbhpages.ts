cf022ca01c212327ef2b1c5b595305b3eabda535
Mary Perez <mperez@accumen.com>
2024-10-02 10:37:13 -0500

    add jsons

--- mbhaddpatient.mbhpages.ts
+++ mbhaddpatient.mbhpages.ts
@@ -1,11 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test} from '@playwright/test';
 import LoginPage from './classes/loginPage';
 import DashboardPage from './classes/dashboardPage';
 import PatientsPage from './classes/patientsPage';
 const logindata = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/login.json")))
-const addpatient = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/addpatient.json")))
-const editpatient = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/editpatient.json")))
-
+const ap = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/addpatient.json")))
+const ep = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/editpatient.json")))
+const nphw = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/newpatienthw.json")))
+const vpd = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/viewpatientdetails.json")))
 
 test('add patient', async ({ page }) => {
 
@@ -17,17 +18,17 @@ test('add patient', async ({ page }) => {
     await login.clickLoginBtn();
   
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown(addpatient.optionClient);
+    await dashboard.clickClientDropDown(ap.optionClient);
 
     const patients = new PatientsPage(page);
     await patients.selectPatients();
-    await patients.addPatient(addpatient.fname,addpatient.lname,addpatient.mrn,addpatient.hhYear,addpatient.hhMonth,addpatient.hhDay,
-        addpatient.gender,addpatient.race,addpatient.ethnicity,addpatient.dobyear,addpatient.dobmonth,addpatient.dobday,addpatient.hippa,addpatient.phone,
-    addpatient.street,addpatient.city,addpatient.state,addpatient.zipcode,addpatient.height,addpatient.weight);
+    await patients.addPatient(ap.fname,ap.lname,ap.mrn,ap.hhYear,ap.hhMonth,ap.hhDay,
+        ap.gender,ap.race,ap.ethnicity,ap.dobyear,ap.dobmonth,ap.dobday,ap.hippa,ap.phone,
+    ap.street,ap.city,ap.state,ap.zipcode,ap.height,ap.weight);
     await patients.savePatient();
 })
 
-test('new patient height and weight test', async ({ page }) => {
+test('new patient height and weight', async ({ page }) => {
 
     test.slow();
     const login = new LoginPage(page);
@@ -37,16 +38,17 @@ test('new patient height and weight test', async ({ page }) => {
     await login.clickLoginBtn();
   
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(nphw.optionClient);
 
     const patients = new PatientsPage(page);
     await patients.selectPatients();
-    await patients.addPatient('Nacho','Libre','365524987','2024','April','25','Male','Other Race','Hispanic','1988','January','18','no','2673196195',
-    '123 Monk House rd','Nowhere','NM','19008','63.4','117.56');
-    await patients.editPatientHeight('-63');
-    await patients.editPatientWeight('-117')
-    await patients.editPatientHeight('63');
-    await patients.editPatientWeight('117');
+    await patients.addPatient(nphw.fname,nphw.lname,nphw.mrn,nphw.hhYear,nphw.hhMonth,nphw.hhDay,
+    nphw.gender,nphw.race,nphw.ethnicity,nphw.dobyear,nphw.dobmonth,nphw.dobday,nphw.hippa,
+    nphw.phone,nphw.street,nphw.city,nphw.state,nphw.zipcode,nphw.height,nphw.weight);
+    await patients.editPatientHeight(nphw.height2);
+    await patients.editPatientWeight(nphw.weight2)
+    await patients.editPatientHeight(nphw.height3);
+    await patients.editPatientWeight(nphw.weight3);
     await patients.savePatient();
 })
 
@@ -60,21 +62,21 @@ test('edit patient', async ({ page }) => {
     await login.clickLoginBtn();
   
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown(editpatient.optionClient);
+    await dashboard.clickClientDropDown(ep.optionClient);
 
     const patients = new PatientsPage(page);
     await patients.selectPatients();
-    await patients.searchPatient(editpatient.patient);
-    await patients.selectPatientfromSearch(editpatient.patient);
-    await patients.editPatientDetails(editpatient.changeDesc);
-    await patients.editPatientRace(editpatient.race);
-    await patients.editPatientEthnicity(editpatient.ethnicity);
-    await patients.editPatientHeight(editpatient.height);
-    await patients.editPatientWeight(editpatient.weight);
+    await patients.searchPatient(ep.patient);
+    await patients.selectPatientfromSearch(ep.patient);
+    await patients.editPatientDetails(ep.changeDesc);
+    await patients.editPatientRace(ep.race);
+    await patients.editPatientEthnicity(ep.ethnicity);
+    await patients.editPatientHeight(ep.height);
+    await patients.editPatientWeight(ep.weight);
     await patients.savePatient();
 })
 
-test('check patient details', async ({ page }) => {
+test('view patient details', async ({ page }) => {
 
     test.slow();
     const login = new LoginPage(page);
@@ -84,13 +86,11 @@ test('check patient details', async ({ page }) => {
     await login.clickLoginBtn();
   
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(vpd.optionClient);
 
     const patients = new PatientsPage(page);
     await patients.selectPatients();
-    await patients.searchPatient('Jack');
-    await patients.selectPatientfromSearch('Jack Black');
+    await patients.searchPatient(vpd.patient);
+    await patients.selectPatientfromSearch(vpd.patient);
     await patients.hoverPatientDetails();
-    await patients.patientVerify(1);
-
 })
