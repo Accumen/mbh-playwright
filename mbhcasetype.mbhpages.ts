7e6c78d89a0a74c1f7460f60a70e45aaf41184cc
Mary Perez <mperez@accumen.com>
2024-11-04 15:58:04 -0600

    added jsons

--- mbhcasetype.mbhpages.ts
+++ mbhcasetype.mbhpages.ts
@@ -3,6 +3,10 @@ import LoginPage from './classes/loginPage';
 import DashboardPage from './classes/dashboardPage';
 import CasetypesPage from './classes/casetypesPage';
 const logindata = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/login.json")))
+const dct = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/deletecasetype.json")))
+const act = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/addcasetype.json")))
+const ect = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/editcasetype.json")))
+const ctf = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/casetypesfeatures.json")))
 
 test('delete case type', async ({ page }) => {
     test.slow();
@@ -14,17 +18,15 @@ test('delete case type', async ({ page }) => {
     await login.clickLoginBtn();
 
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(dct.optionClient);
 
     const casetypes = new CasetypesPage(page);
     await casetypes.selectCaseTypeMenu();
-    await casetypes.searchCaseType('123');
+    await casetypes.searchCaseType(dct.searchCase);
     await casetypes.deleteCaseType();
-    await casetypes.clearSelections();
-    await casetypes.searchCaseType('123');
 })
 
-test('add/save case type', async ({page})=> {
+test('add case type', async ({page})=> {
     test.slow();
     const login = new LoginPage(page);
 
@@ -34,106 +36,53 @@ test('add/save case type', async ({page})=> {
     await login.clickLoginBtn();
 
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(act.optionClient);
 
     const casetypes = new CasetypesPage(page);
     await casetypes.selectCaseTypeMenu();
     await casetypes.clickAddCaseType();
-    await casetypes.newCaseType('123','3mL/h','test case for playwright','Surgical','VASCULAR','Active');
+    await casetypes.newCaseType(act.name,act.bloodloss,act.description,act.worklisttype,act.parentcasetype,act.status);
+    await casetypes.saveCaseType();
+})
+
+test('edit case type', async ({page})=> {
+    test.slow();
+    const login = new LoginPage(page);
+
+    await page.goto('https://qa-auto-base.mybloodhealth.com/login');
+    await login.enterEmail(logindata.email);
+    await login.enterPassword(logindata.password);
+    await login.clickLoginBtn();
+
+    const dashboard = new DashboardPage(page);
+    await dashboard.clickClientDropDown(ect.optionClient);
+
+    const casetypes = new CasetypesPage(page);
+    await casetypes.selectCaseTypeMenu();
+    await casetypes.searchCaseType(ect.searchCase);
+    await casetypes.selectCaseTypeName(ect.searchCase);
+    await casetypes.editWorklistType(ect.worklisttype);
+    await casetypes.saveCaseType();
+
+})
+
+test('case types features', async ({page})=> {
+    test.slow();
+    const login = new LoginPage(page);
+
+    await page.goto('https://qa-auto-base.mybloodhealth.com/login');
+    await login.enterEmail(logindata.email);
+    await login.enterPassword(logindata.password);
+    await login.clickLoginBtn();
+
+    const dashboard = new DashboardPage(page);
+    await dashboard.clickClientDropDown(ctf.optionClient);
+
+    const casetypes = new CasetypesPage(page);
+    await casetypes.selectCaseTypeMenu();
+    await casetypes.adjustRowCount(ctf.row);
+    await casetypes.statusDropDown(ctf.status);
+    await casetypes.clearSelections();
+    await casetypes.selectCaseTypeName(ctf.searchCase);
     await casetypes.backArrow();
-    await casetypes.clickAddCaseType();
-    await casetypes.newCaseType('123','3mL/h','test case for playwright','Surgical','VASCULAR','Active');
-    await casetypes.saveCaseType();
-})
-
-test('search/edit case type', async ({page})=> {
-    test.slow();
-    const login = new LoginPage(page);
-
-    await page.goto('https://qa-auto-base.mybloodhealth.com/login');
-    await login.enterEmail(logindata.email);
-    await login.enterPassword(logindata.password);
-    await login.clickLoginBtn();
-
-    const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
-
-    const casetypes = new CasetypesPage(page);
-    await casetypes.selectCaseTypeMenu();
-    await casetypes.searchCaseType('123');
-    await casetypes.statusDropDown('Inactive');
-    await casetypes.clearSelections();
-    await casetypes.searchCaseType('123');
-    await casetypes.selectCaseTypeName('123');
-    await casetypes.editWorklistType('Non-Surgical');
-    await casetypes.saveCaseType();
-
-})
-
-test('check parent case type match', async ({page})=> {
-    test.slow();
-    const login = new LoginPage(page);
-
-    await page.goto('https://qa-auto-base.mybloodhealth.com/login');
-    await login.enterEmail(logindata.email);
-    await login.enterPassword(logindata.password);
-    await login.clickLoginBtn();
-
-    const casetypes = new CasetypesPage(page);
-    await casetypes.selectCaseTypeMenu();
-    await casetypes.selectCaseTypeName('ORTHO');
-    await casetypes.caseTypeScreenshot(1);
-
-    const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
-
-    await casetypes.selectCaseTypeMenu();
-    await casetypes.selectCaseTypeName('ORTHO');
-    await casetypes.caseTypeScreenshot(2);
-
-
-})
-
-test('check parent case type match after save/sync', async ({page})=> {
-    test.slow();
-    const login = new LoginPage(page);
-
-    await page.goto('https://qa-auto-base.mybloodhealth.com/login');
-    await login.enterEmail(logindata.email);
-    await login.enterPassword(logindata.password);
-    await login.clickLoginBtn();
-
-    const casetypes = new CasetypesPage(page);
-    await casetypes.selectCaseTypeMenu();
-    await casetypes.selectCaseTypeName('ORTHO');
-    await casetypes.editParentCaseType('ANY OPEN ABDOMINAL PROCEDURE');
-    await casetypes.saveCaseType();
-    await casetypes.selectCaseTypeName('ORTHO');
-    await casetypes.caseTypeScreenshot(1);
-
-    const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
-
-    await casetypes.selectCaseTypeMenu();
-    await casetypes.searchCaseType('ORTHO');
-    await casetypes.syncCaseType();
-    await casetypes.selectCaseTypeName('ORTHO');
-    await casetypes.caseTypeScreenshot(2);
-})
-
-test('adjust row count', async ({page})=> {
-    test.slow();
-    const login = new LoginPage(page);
-
-    await page.goto('https://qa-auto-base.mybloodhealth.com/login');
-    await login.enterEmail(logindata.email);
-    await login.enterPassword(logindata.password);
-    await login.clickLoginBtn();
-
-    const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
-
-    const casetypes = new CasetypesPage(page);
-    await casetypes.selectCaseTypeMenu();
-    await casetypes.adjustRowCount('30');
 })
--- mbhdocuments.mbhpages.ts
+++ mbhdocuments.mbhpages.ts
@@ -6,6 +6,12 @@ import WorklistPage from './classes/worklistPage';
 const logindata = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/login.json")))
 const ud = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/uploaddocument.json")))
 const pd = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/previewdocument.json")))
+const ad = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/adddocument.json")))
+const ed = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/editdocument.json")))
+const dd = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/deletedocument.json")))
+const dsf = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/documentsearchfeature.json")))
+const dp = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/documentpagination.json")))
+const drc = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/documentrowcount.json")))
 const vdr = JSON.parse(JSON.stringify(require("../mbh-playwright/testdata/visitdocumentretention.json")))
 
 test('upload document', async ({ page }) => {
@@ -55,14 +61,12 @@ test('add document', async ({ page }) => {
     await login.clickLoginBtn();
 
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(ad.optionClient);
 
     const document = new DocumentsPage (page);
     await document.selectDocuments();
     await document.addDocBtn();
-    await document.backArrow();
-    await document.addDocBtn();
-    await document.addEditDoc('Second Test Document','Playwrights second test document','Assessment Template','Non-Surgical','Active')
+    await document.addEditDoc(ad.docname,ad.desc,ad.doctype,ad.casetype,ad.docstat)
     await document.saveDoc();
 })
 test('edit document', async ({ page }) => {
@@ -75,16 +79,16 @@ test('edit document', async ({ page }) => {
     await login.clickLoginBtn();
 
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(ed.optionClient);
 
     const document = new DocumentsPage (page);
     await document.selectDocuments();
-    await document.searchDoc('Iron Patient Letter');
-    await document.selectDocFromList('Iron Patient Letter');
-    await document.addSmartSection('Test Smart Section');
+    await document.searchDoc(ed.document);
+    await document.selectDocFromList(ed.docname);
+    await document.addSmartSection(ed.smartdoctype);
     await document.addDocBtn();
-    await document.smartSecInfo('Test Smart Section','This is a test of smart.');
-    await document.addTextSection('Text Section Name','Playwright text section','This is a test for playwright.');
+    await document.smartSecInfo(ed.smartsecname,ed.smartsecdesc);
+    await document.addTextSection(ed.secname,ed.secdesc,ed.seccontent);
     await document.saveDoc();
 })
 test('delete document', async ({ page }) => {
@@ -97,11 +101,11 @@ test('delete document', async ({ page }) => {
     await login.clickLoginBtn();
 
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(dd.optionClient);
 
     const document = new DocumentsPage (page);
     await document.selectDocuments();
-    await document.searchDoc('Second Test Document');
+    await document.searchDoc(dd.document);
     await document.delete();
 })
 test('document search feature', async ({ page }) => {
@@ -114,12 +118,14 @@ test('document search feature', async ({ page }) => {
     await login.clickLoginBtn();
 
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(dsf.optionClient);
 
     const document = new DocumentsPage (page);
     await document.selectDocuments();
-    await document.searchDoc('Second Test Document');
-    await document.docStatusdropdown('Inactive');
+    await document.searchDoc(dsf.document);
+    await document.selectDocFromList(dsf.docname);
+    await document.backArrow();
+    await document.docStatusdropdown(dsf.docstat);
     await document.clearSelections();
 })
 
@@ -133,17 +139,17 @@ test('document pagination', async ({ page }) => {
     await login.clickLoginBtn();
 
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(dp.optionClient);
 
     const document = new DocumentsPage (page);
     await document.selectDocuments();
-    await document.documentPagination(3);
-    await document.selectDocFromList('Surgeon Letter for the Abnormal Patient');
+    await document.documentPagination(dp.num);
+    await document.selectDocFromList(dp.docname);
     await document.backArrow();
     await document.paginationCheck();
 })
 
-test('document pagination dropdown', async ({ page }) => {
+test('document row count', async ({ page }) => {
     test.slow();
     const login = new LoginPage(page);
 
@@ -153,15 +159,16 @@ test('document pagination dropdown', async ({ page }) => {
     await login.clickLoginBtn();
 
     const dashboard = new DashboardPage(page);
-    await dashboard.clickClientDropDown('QA Testing');
+    await dashboard.clickClientDropDown(drc.optionClient);
 
     const document = new DocumentsPage (page);
     await document.selectDocuments();
-    await document.adjustRowCount('30');
+    await document.adjustRowCount(drc.row);
+    await document.selectDocFromList(drc.docname);
 })
 
 test('visit document retention', async ({page}) =>{
-    //test.slow();
+    
     test.setTimeout(120000);
     const login = new LoginPage(page);
 
