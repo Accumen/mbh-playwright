e96b7b11802a12bb1e31a5adff20d0a07157f479
Mary Perez <mperez@accumen.com>
2024-12-09 10:05:54 -0600

    updated for nightly run

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,24 +15,19 @@ const config: PlaywrightTestConfig = {
     testDir: "./",
     fullyParallel: true,
 use: {
-    //baseURL: "qa.mybloodhealth.com",
-    headless: false, //"false" runs with a visible UI
+    //baseURL: "qa-auto-base.mybloodhealth.com",
+    headless: true, //"false" runs with a visible UI
     screenshot: "on", // takes a screenshot (options are "on", "off", "only-on-failure")
     video:"on", // takes a video  (options are "on", "on first retry", "retain-on-failure", "retry-with-video")
     launchOptions: { // tells it to perform the steps in a slow manner
         slowMo: 1000
     }
 },
-workers: 2,
+//workers: 2,
 retries: 0, //this will retry to run the test automatically 2x if the test fails the first time
 reporter: [["dot"], ["json", { //This tells it to run reports
-}],["html"]]
-    ouputFile: "jsonReports/jsonFile.json" // where to put the output
-}],["html", {
-    open: "never" // tells when to open the html report (options are "always", "never", and "on-failure")
-}],
+}],["html"],
 ["junit", {outputFile: 'results.xml'}]]
-
 };
 
 export default config;
